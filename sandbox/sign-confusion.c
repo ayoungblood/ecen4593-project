0fc483c75d89a5d0889235f7885bd21f8807b549
Akira Youngblood <akirayoungblood@me.com>
2017-02-27 22:34:06 -0700

    Trying to understand subtraction

--- sandbox/sign-confusion.c
+++ sandbox/sign-confusion.c
@@ -9,6 +9,21 @@
 #define HIBIT(x) (((uint32_t)x)>>31)
 
 int main(int argc, char *argv[]){
+    int i,j;
+    int8_t a8, b8;
+    int16_t a16, b16;
+    for (i = CHAR_MIN; i <= CHAR_MAX; ++i) {
+        for (j = CHAR_MIN; j <= CHAR_MAX; ++j) {
+            a16 = (int16_t) i;
+            a8 = (int8_t) i;
+            b16 = (int16_t) j;
+            b8 = (int8_t) j;
+            if (((a8 - b8) & 0xff) != ((a16 - b16) & 0xff)) {
+                printf("OVERFLOW: i=%d,j=%d\n",i,j);
+            }
+        }
+    }
+    return 0;
     int32_t i32 = INT_MIN;
     do {
         if (i32 < 0) {
