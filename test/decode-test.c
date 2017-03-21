c6c1ba3ad263c2395330391747ff00f98255b8a7
Akira Youngblood <akirayoungblood@me.com>
2017-03-21 10:39:51 -0600

    Fixing memory alloc issue, decode-test doesn't segfault no more

--- test/decode-test.c
+++ test/decode-test.c
@@ -16,22 +16,32 @@ int tests_run = 0;
 int flags = MASK_DEBUG | MASK_VERBOSE | MASK_SANITY;
 
 word_t i, p;
-control_t *idex, *ifid;
+control_t *ifid, *idex, *dummy_exmem, *dummy_memwb;
+bool dummy_stall;
+pc_t dummy_pc;
 
 /* Some R-Type instructions*/
 
 static char * test_decode_add() {
     i = 0x02518820; // add, $s1, $s2, $s1
     p = 0x4;
-    idex = (control_t *)malloc(sizeof(control_t));
-    ifid = (control_t *)malloc(sizeof(control_t));
+    // Initialize/clear pipeline registers
+    pipeline_init(&ifid, &idex, &dummy_exmem, &dummy_memwb, &dummy_pc, &dummy_stall, 0);
+    //idex = (control_t *)malloc(sizeof(control_t));
+    //ifid = (control_t *)malloc(sizeof(control_t));
+    //idex->regName = (char*)malloc(7*sizeof(char));
+    //ifid->regName = (char*)malloc(7*sizeof(char));
+    //idex->regName = "ID/EX";
+    //ifid->regName = "IF/ID";
     ifid->instr = OPC_RTYPE;
     ifid->regRs = REG_S2;
     ifid->regRt = REG_S1;
     ifid->regRd = REG_S1;
     ifid->funct = FNC_ADD;
     ifid->pcNext = p + 4;
+    printf("post init test_decode_add()\n");
     decode(ifid, idex);
+    printf("post decode test_decode_add()\n");
     mu_assert(_FL "instruction add, $s1, $s2, $s1 bad regDst", idex->regDst == 1);
     mu_assert(_FL "instruction add, $s1, $s2, $s1 badregWrite", idex->regWrite == 1);
     mu_assert(_FL "instruction add, $s1, $s2, $s1 bad ALUSrc", idex->ALUSrc == 0);
@@ -44,8 +54,9 @@ static char * test_decode_add() {
     mu_assert(_FL "instruction add, $s1, $s2, $s1 bad pcNext", idex->pcNext == 0x8);
     mu_assert(_FL "instruction add, $s1, $s2, $s1 bad regRsValue", idex->regRsValue == 18);
     mu_assert(_FL "instruction add, $s1, $s2, $s1 bad regRtValue", idex->regRtValue == 17);
-    free(idex);
-    free(ifid);
+    //free(idex);
+    //free(ifid);
+    pipeline_destroy(&ifid, &idex, &dummy_exmem, &dummy_memwb);
     return 0;
 }
 
@@ -54,8 +65,10 @@ static char * test_decode_add() {
 static char * test_decode_addi() {
     i = 0x22a8ff9c;                  //addi $t0, $s5, -100
     p = 0x4;
-    idex = (control_t *)malloc(sizeof(control_t));
-    ifid = (control_t *)malloc(sizeof(control_t));
+    // Initialize/clear pipeline registers
+    pipeline_init(&ifid, &idex, &dummy_exmem, &dummy_memwb, &dummy_pc, &dummy_stall, 0);
+    //idex = (control_t *)malloc(sizeof(control_t));
+    //ifid = (control_t *)malloc(sizeof(control_t));
     ifid->opCode = OPC_ADDI;
     ifid->regRs = REG_S5;
     ifid->regRt = REG_T0;
@@ -72,8 +85,9 @@ static char * test_decode_addi() {
     mu_assert(_FL "instruction addi $t0, $s5, -100 bad jump", idex->jump == 0);
     mu_assert(_FL "instruction addi $t0, $s5, -100 bad pcNext", idex->pcNext == 0x8);
     mu_assert(_FL "instruction addi $t0, $s5, -100 bad regRsValue", idex->regRsValue == 21);
-    free(idex);
-    free(ifid);
+    //free(idex);
+    //free(ifid);
+    pipeline_destroy(&ifid, &idex, &dummy_exmem, &dummy_memwb);
     return 0;
 }
 
@@ -82,8 +96,10 @@ static char * test_decode_beq() {
     i = 0x12110fff;                  //beq $s0, $s1, 0x4000
     p = 0x4;
     uint32_t temp = 14;
-    idex = (control_t *)malloc(sizeof(control_t));
-    ifid = (control_t *)malloc(sizeof(control_t));
+    // Initialize/clear pipeline registers
+    pipeline_init(&ifid, &idex, &dummy_exmem, &dummy_memwb, &dummy_pc, &dummy_stall, 0);
+    //idex = (control_t *)malloc(sizeof(control_t));
+    //ifid = (control_t *)malloc(sizeof(control_t));
     ifid->opCode = OPC_BEQ;
     ifid->regRs = REG_S0;
     ifid->regRt = REG_S1;
@@ -117,9 +133,9 @@ static char * test_decode_beq() {
     mu_assert(_FL "instruction beq $s0, $s1, 0x4000 (not equal) ALUop", idex->ALUop == OPR_SUB);
     mu_assert(_FL "instruction beq $s0, $s1, 0x4000 (not equal) jump", idex->jump == 0);
     mu_assert(_FL "instruction beq $s0, $s1, 0x4000 (not equal) pcNext", idex->pcNext == 0x8);
-    free(ifid);
-    free(idex);
-
+    //free(ifid);
+    //free(idex);
+    pipeline_destroy(&ifid, &idex, &dummy_exmem, &dummy_memwb);
     return 0;
 }
 
@@ -129,8 +145,10 @@ static char * test_decode_bne() {
     i = 0x1483fff7;                  //bne $a0, $v1, -32
     printf("Instruction: bne $a0, $v1, -32 (taken)\n");
     p = 0x40006020;
-    ifid = (control_t *)malloc(sizeof(control_t));
-    idex = (control_t *)malloc(sizeof(control_t));
+    // Initialize/clear pipeline registers
+    pipeline_init(&ifid, &idex, &dummy_exmem, &dummy_memwb, &dummy_pc, &dummy_stall, 0);
+    //ifid = (control_t *)malloc(sizeof(control_t));
+    //idex = (control_t *)malloc(sizeof(control_t));
     uint32_t temp = 0;
     reg_write(REG_A0, &temp);
     temp = 5;
@@ -166,9 +184,9 @@ static char * test_decode_bne() {
 
 
 
-    free(ifid);
-    free(idex);
-
+    //free(ifid);
+    //free(idex);
+    pipeline_destroy(&ifid, &idex, &dummy_exmem, &dummy_memwb);
     return 0;
 }
 
@@ -178,8 +196,10 @@ static char * test_decode_bne() {
 static char * test_decode_lw() {
     i = 0x8d120005;                  //lw $s2, 4($t0)
     p = 0x4;
-    ifid = (control_t *)malloc(sizeof(control_t));
-    idex = (control_t *)malloc(sizeof(control_t));
+    // Initialize/clear pipeline registers
+    pipeline_init(&ifid, &idex, &dummy_exmem, &dummy_memwb, &dummy_pc, &dummy_stall, 0);
+    //ifid = (control_t *)malloc(sizeof(control_t));
+    //idex = (control_t *)malloc(sizeof(control_t));
     ifid->opCode = OPC_LW;
     ifid->regRs = REG_T0;
     ifid->regRt = REG_S2;
@@ -197,9 +217,9 @@ static char * test_decode_lw() {
     mu_assert(_FL "Instruction: lw $s2, 4($t0) ALUop", idex->ALUop == OPR_ADDU);
     mu_assert(_FL "Instruction: lw $s2, 4($t0) jump", idex->jump == 0);
     mu_assert(_FL "Instruction: lw $s2, 4($t0) pcNext", idex->pcNext == 0x8);
-    free(ifid);
-    free(idex);
-
+    //free(ifid);
+    //free(idex);
+    pipeline_destroy(&ifid, &idex, &dummy_exmem, &dummy_memwb);
     return 0;
 }
 
@@ -208,8 +228,10 @@ static char * test_decode_lw() {
 static char * test_decode_sw() {
     i = 0xad1f0000;                  //sw $ra, 0($t0)
     p = 0x4;
-    ifid = (control_t *)malloc(sizeof(control_t));
-    idex = (control_t *)malloc(sizeof(control_t));
+    // Initialize/clear pipeline registers
+    pipeline_init(&ifid, &idex, &dummy_exmem, &dummy_memwb, &dummy_pc, &dummy_stall, 0);
+    //ifid = (control_t *)malloc(sizeof(control_t));
+    //idex = (control_t *)malloc(sizeof(control_t));
     ifid->opCode = OPC_SW;
     ifid->regRs = REG_T0;
     ifid->regRt = REG_RA;
@@ -225,9 +247,9 @@ static char * test_decode_sw() {
     mu_assert(_FL "Instruction: sw $ra, 0($t0) jump", idex->jump == 0);
     mu_assert(_FL "Instruction: sw $ra, 0($t0) pcNext", idex->pcNext == 0x8);
 
-    free(ifid);
-    free(idex);
-
+    //free(ifid);
+    //free(idex);
+    pipeline_destroy(&ifid, &idex, &dummy_exmem, &dummy_memwb);
     return 0;
 }
 
@@ -236,8 +258,10 @@ static char * test_decode_sw() {
 static char * test_decode_slti() {
     i = 0x2a28f000;                  //slti $t0, $s1, 0xf000
     p = 0x4;
-    ifid = (control_t *)malloc(sizeof(control_t));
-    idex = (control_t *)malloc(sizeof(control_t));
+    // Initialize/clear pipeline registers
+    pipeline_init(&ifid, &idex, &dummy_exmem, &dummy_memwb, &dummy_pc, &dummy_stall, 0);
+    //ifid = (control_t *)malloc(sizeof(control_t));
+    //idex = (control_t *)malloc(sizeof(control_t));
     ifid->opCode = OPC_SLTI;
     ifid->regRs = REG_S1;
     ifid->regRt = REG_T0;
@@ -254,9 +278,9 @@ static char * test_decode_slti() {
     mu_assert(_FL "slti $t0, $s1, 0xf000 ALUop", idex->ALUop == OPR_SLT);
     mu_assert(_FL "slti $t0, $s1, 0xf000 jump", idex->jump == 0);
     mu_assert(_FL "slti $t0, $s1, 0xf000 pcNext", idex->pcNext == 0x8);
-    free(ifid);
-    free(idex);
-
+    //free(ifid);
+    //free(idex);
+    pipeline_destroy(&ifid, &idex, &dummy_exmem, &dummy_memwb);
     return 0;
 }
 
@@ -264,8 +288,10 @@ static char * test_decode_slti() {
 static char * test_decode_sltiu() {
     i = 0x2e28f000;                  //sltiu $t0, $s1, 0xf000
     p = 0x4;
-    ifid = (control_t *)malloc(sizeof(control_t));
-    idex = (control_t *)malloc(sizeof(control_t));
+    // Initialize/clear pipeline registers
+    pipeline_init(&ifid, &idex, &dummy_exmem, &dummy_memwb, &dummy_pc, &dummy_stall, 0);
+    //ifid = (control_t *)malloc(sizeof(control_t));
+    //idex = (control_t *)malloc(sizeof(control_t));
     ifid->opCode = OPC_SLTIU;
     ifid->regRs = REG_S1;
     ifid->regRt = REG_T0;
@@ -282,9 +308,9 @@ static char * test_decode_sltiu() {
     mu_assert(_FL "sltiu $t0, $s1, 0xf000 ALUop", idex->ALUop == OPR_SLTU);
     mu_assert(_FL "sltiu $t0, $s1, 0xf000 jump", idex->jump == 0);
     mu_assert(_FL "sltiu $t0, $s1, 0xf000 pcNext", idex->pcNext == 0x8);
-    free(ifid);
-    free(idex);
-
+    //free(ifid);
+    //free(idex);
+    pipeline_destroy(&ifid, &idex, &dummy_exmem, &dummy_memwb);
     return 0;
 }
 
@@ -293,8 +319,10 @@ static char * test_decode_sltiu() {
 static char * test_decode_j() {
     i = 0x08000804;                  //j 0x2011
     p = 0x600A1100;
-    ifid = (control_t *)malloc(sizeof(control_t));
-    idex = (control_t *)malloc(sizeof(control_t));
+    // Initialize/clear pipeline registers
+    pipeline_init(&ifid, &idex, &dummy_exmem, &dummy_memwb, &dummy_pc, &dummy_stall, 0);
+    //ifid = (control_t *)malloc(sizeof(control_t));
+    //idex = (control_t *)malloc(sizeof(control_t));
     ifid->opCode = OPC_J;
     ifid->address = 0x00002010;
     ifid->pcNext = p + 4;
@@ -310,18 +338,17 @@ static char * test_decode_j() {
     mu_assert(_FL "j 0x2011 jump", idex->jump == 1);
     mu_assert(_FL "j 0x2011 address", idex->address == 0x00008040);
     mu_assert(_FL "j 0x2011 pcNext", idex->pcNext == 0x60008040);
-    free(ifid);
-    free(idex);
+    //free(ifid);
+    //free(idex);
 
-
+    pipeline_destroy(&ifid, &idex, &dummy_exmem, &dummy_memwb);
     return 0;
 }
 
 static char * all_tests() {
 
+    // Initialize the register file with some values
     reg_init();
-
-    //Initialize the register file with some values
     uint32_t i = 0;
     for(i = 0; i < 32; i++){
         reg_write(i, &i);
@@ -336,7 +363,6 @@ static char * all_tests() {
     mu_run_test(test_decode_slti);
     mu_run_test(test_decode_sltiu);
     mu_run_test(test_decode_j);
-
 
     return 0;
 }
